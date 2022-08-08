4e01e13b323b0cfda46ac9c6e6036caa80c32b9e
pradeep <ppadmukhi28@gmail.com>
2022-08-08 12:46:52 +0530

    socket.io connection with small example

--- src/index.js
+++ src/index.js
@@ -1,9 +1,11 @@
 const express = require('express')
 const path = require('path')
 const http = require('http');
+const socketio = require('socket.io');
 
 const app = express()
 const server = http.createServer(app)
+const io = socketio(server)
 
 const port = process.env.PORT || 3000 
 
@@ -11,6 +13,20 @@ app.use(express.json())
 
 const publicDirPath = path.join(__dirname, '../public')
 app.use(express.static(publicDirPath));
+
+let count = 0;
+
+io.on('connection', (socket) => {
+    console.log("new websocket connection");
+
+    socket.emit('countUpdated', count)
+
+    socket.on('increament', () => {
+        count++
+        // socket.emit('countUpdated', count)  // this will update  only single connection count
+        io.emit('countUpdated', count) // this will update for all the connections
+    })
+})
 
 
 server.listen(port, () => {
